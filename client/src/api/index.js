2478ea34c0435be72332cb1736dcf3263756e420
sforsanjnarao <sanjnarao26@gmail.com>
2025-04-08 12:38:44 +0530

    update API base URL to production endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "localholt:5002",
+  baseURL: "https://stack-overflow-eosin-delta.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
